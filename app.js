863e732efff4b20d226bf9fa8b4ca0ab8e5aa0fb
Komilbek Bakhodirov <komilbek.bakhodirov@gmail.com>
2022-06-29 22:36:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const notFound = require('./middleware/not-found');
+require('dotenv').config();
+const connectDB = require('./db/connect');
+const express = require('express');
+const tasks = require('./routes/tasks');
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.static('./public'));
+
+// routes
+app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`Listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
